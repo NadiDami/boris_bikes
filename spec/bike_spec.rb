ea78925bd0355416b06fab8a38b4a0f9d76b6c7e
Nadia Odunayo <nodunayo@gmail.com>
2013-10-14 22:54:52 +0100

    Minor refactoring

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,9 +2,20 @@ require 'bike'
 
 describe Bike do
 
+  let(:bike) {Bike.new}
+
   it 'should not be broken after we create it' do
-    bike = Bike.new
     expect(bike.broken?).to be_false
   end
   
+  it 'should be able to break' do
+    bike.break!
+    expect(bike.broken?).to be_true
+  end
+
+  it 'can be repaired' do
+    bike.break!
+    bike.fix!
+    expect(bike).not_to be_broken
+  end
 end
