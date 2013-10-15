eeecc017f7b710c31c0f0a75b46b89be39fd6cba
Nadia Odunayo <nodunayo@gmail.com>
2013-10-15 08:39:27 +0100

    Modified docking station files

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,8 @@ require 'docking_station'
 
 describe DockingStation do
 
+  let(:bike) { Bike.new }
+  let(:station) { DockingStation.new }
   it 'should accept a bike' do
     bike = Bike.new
     station = DockingStation.new
@@ -9,5 +11,12 @@ describe DockingStation do
     station.dock(bike)
     expect(station.bike_count).to eq 1
   end
+
+  it 'should release a bike' do
+    station.dock(bike)
+    station.release(bike)
+    expect(station.bike_count). to eq 0
+  end
+
 
 end
